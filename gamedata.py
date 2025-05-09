1fb668c0d1e5eb1c50381158a1a9cc03071dc670
shillianne <vshiwork@gmail.com>
2025-05-09 17:30:41 +0200

    Corrected the correction

--- gamedata.py
+++ gamedata.py
@@ -112,7 +112,7 @@ class GameDataCollector:
         else:
             # si hay juegos guardados, el id es el siguiente
             # game_id = len(os.listdir(self.output_dir))
-            game_id = f(sorted(os.listdir(self.output_dir), key=f)[-1])
+            game_id = f(sorted(os.listdir(self.output_dir), key=f)[-1]) + 1
         # Crear el nombre del archivo
         # del timestamp solo nos quedamos con la fecha dia/mes/año
         steps_filename = os.path.join(self.output_dir, f"game_{game_id}.csv")
