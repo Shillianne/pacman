a0b549048646fa2771a088bb632e081148e90e03
shillianne <vshiwork@gmail.com>
2025-05-10 22:45:00 +0200

    Corrected error by Ahmed

--- game.py
+++ game.py
@@ -767,7 +767,7 @@ class Game:
             # Ahmed 
             ###################################################
             # AQUÍ es donde capturamos los datos
-            if self.data_collector:
+            if self.data_collector and agentIndex == 0:
                 self.data_collector.capture_step(agentIndex, current_state, action, self.state)
             ###################################################
 
